9c8c0978b6a6c75bf794e7cd9e72f51c487e3bf1
viraj159 <virajchoksi276@gmail.com>
2025-04-30 16:01:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,11 @@ def get_path(parents, destination):
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+    path = []
+    while destination in parents:
+        parent = parents[destination]
+        path.append(parent)
+        destination = parent
+    path.reverse()
+    return '->'.join(path)
 
